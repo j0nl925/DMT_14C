86e19ba89e19a86ca75d42fabdeb090abf985717
imrankhan37 <63012637+imrankhan37@users.noreply.github.com>
2023-01-12 11:57:32 +0000

    update #5

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from DAQDataCollection import main
-
 import DAQDataCollection
 
+no_of_voltage_channels = 4
+voltage_sampling_rate = 5000
+no_of_voltage_sample = 500
+min_voltage = 0
+max_voltage = 5
 
+no_of_temperature_channels = 2
+temperature_sampling_rate =  95
+no_of_temperature_samples = 75
+
+no_of_strain_channels = 2
+strain_sampling_rate =  500
+no_of_strain_samples = 50
 
 if __name__ == '__main__':
-    main(4, 3000, 1000, 0, 5,
-         2, 3000, 1000,
-         2, 3000, 1000)
+    main(no_of_voltage_channels, voltage_sampling_rate, no_of_voltage_sample, min_voltage, max_voltage,
+        no_of_temperature_channels, temperature_sampling_rate, no_of_temperature_samples,
+         no_of_strain_channels, strain_sampling_rate, no_of_strain_samples)
